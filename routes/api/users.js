e8689f5ccda960c12e6ff4a63b8dccb1a388d6cd
Logan <lpotts159@gmail.com>
2020-09-01 15:13:15 -0400

    complete user verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const User = require("../../models/User");
 
 const router = express.Router();
@@ -12,27 +16,77 @@ router
 //  @access     Public
 .post("/",
     [
-        check("name", "Name is required").not().notEmpty(),
+        check("name", "Name is required").notEmpty(),
         check("email", "A valid email is required").isEmail(),
         check("password", "A password length of 6 or more is required").isLength({ min: 6 })
     ],
     async (req, res) => {
-        const { name, email, password } = req.body;
         const errors = validationResult(req);
 
-        !errors.isEmpty() && res.status(400).json({ errors: errors.array() });
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+
+        const { name, email, password } = req.body;
+
 
         try {
             const existingUser = await User.findOne({ email });
 
-            if(existingUser) {
-                res.status(400).json({ errors: [{ msg: "Email address is already in use" }] });
+            if (existingUser) {
+                return res.status(400).json({ errors: [{ msg: "Email address is already in use" }] });
             }
+
+
+            // Use gravatar to get avatar from email
+            const avatar = gravatar.url(
+                email, 
+                {
+                s: "200", // size
+                r: "pg", // rating
+                d: "mm" // default
+            });
+
+            const user = new User({
+                name,
+                email,
+                password,
+                avatar
+            });
+
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+
+            await user.save();
+
             
-            res.send("User Route");
+            // Create jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const secret = config.get("jwtSecret");
+
+            jwt.sign(
+                payload,
+                secret,
+                { expiresIn: 3600000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+
         } catch(err) {
             console.log(err.message);
-            res.status(500, "The server is having some issues");
+            return res.status(500, "The server is having some issues");
         }
         
     }
