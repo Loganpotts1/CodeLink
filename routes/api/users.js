5975e087b0247448793f7fc18514bbe474e38b4d
Logan <lpotts159@gmail.com>
2020-08-31 22:47:07 -0400

    refactoring code

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+
 const router = express.Router();
 
 
@@ -14,13 +16,25 @@ router
         check("email", "A valid email is required").isEmail(),
         check("password", "A password length of 6 or more is required").isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
+        const { name, email, password } = req.body;
         const errors = validationResult(req);
 
         !errors.isEmpty() && res.status(400).json({ errors: errors.array() });
 
-        console.log(req.body);
-        res.send("User Route");
+        try {
+            const existingUser = await User.findOne({ email });
+
+            if(existingUser) {
+                res.status(400).json({ errors: [{ msg: "Email address is already in use" }] });
+            }
+            
+            res.send("User Route");
+        } catch(err) {
+            console.log(err.message);
+            res.status(500, "The server is having some issues");
+        }
+        
     }
 );
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/database");
+const connectDB = require("./config/connectDB");
 
 const app = express();
 
