2d75cec1d530e1db4dc1b1539a62b88ffdafc964
Logan <lpotts159@gmail.com>
2020-09-13 23:25:30 -0400

    adding Router and LandingPage Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar />
+        <Route exact path="/" component={ Landing }/>
+    </Router>
   );
 }
 
---
+++ client/src/layout/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Landing from "../components/Landing";
+
+export default function LandingPage() {
+
+    return(
+        <div className="container">
+            <Navbar />
+            <Landing />
+        </div>
+    );
+}
