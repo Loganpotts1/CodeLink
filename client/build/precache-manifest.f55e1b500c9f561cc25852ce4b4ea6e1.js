62a48f28f35e8cebfd7a9d92cb61bb2c1b96aa0f
Logan <lpotts159@gmail.com>
2021-01-11 13:04:40 -0500

    npm run build

---
+++ client/build/precache-manifest.f55e1b500c9f561cc25852ce4b4ea6e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8599c354d71ca9982e88bec665667f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc75519489fec697892",
+    "url": "/static/css/main.efb259c4.chunk.css"
+  },
+  {
+    "revision": "a56df5fe64d96f6d1d5b",
+    "url": "/static/js/2.d5d25776.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5d25776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc75519489fec697892",
+    "url": "/static/js/main.2d6340b1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1e4739977b0728149a89ea9ac248763c",
+    "url": "/static/media/CodeLink_Guest_Icon.1e473997.png"
+  },
+  {
+    "revision": "14400fc47471a007ab08e9f64a2cd518",
+    "url": "/static/media/banner.14400fc4.jpg"
+  },
+  {
+    "revision": "d9c97c59eee64b5461b805f384e2a3a2",
+    "url": "/static/media/showcase.d9c97c59.jpg"
+  },
+  {
+    "revision": "8a0896ae2baf9d5b13b2020a738eab68",
+    "url": "/static/media/spinner.8a0896ae.gif"
+  }
+]);
