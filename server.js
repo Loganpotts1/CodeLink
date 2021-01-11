885eb5d38d143bf9d784b2955c7a230be01728f7
Logan <lpotts159@gmail.com>
2021-01-11 14:47:45 -0500

    testing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,11 +14,7 @@ connectDB();
 
 
 //  CORS HEADER
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 
 app.get("/", (req, res) => res.send("API Running"));
